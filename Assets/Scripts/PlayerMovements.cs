07b4de519ddd749939521487198a1a7bb17cd586
Farou <86537710+Fakhri-Amin@users.noreply.github.com>
2023-06-19 16:43:32 +0900

    Made better and clearer tutorial page

--- Assets/Scripts/PlayerMovements.cs
+++ Assets/Scripts/PlayerMovements.cs
@@ -2,11 +2,23 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerMovements : MonoBehaviour
+using MG_BlocksEngine2.Block.Instruction;
+using MG_BlocksEngine2.Block;
+
+using MG_BlocksEngine2.Core;
+using MG_BlocksEngine2.Environment;
+
+public class PlayerMovements : BE2_InstructionBase
 {
     private bool isMoving;
     private Vector3 origPos, targetPos;
-    private float timeToMove = 0.2f;
+    private float timeToMove = 0.5f;
+    private Animator animator;
+
+    private void Awake()
+    {
+        animator = GetComponent<Animator>();
+    }
 
     // Start is called before the first frame update
     void Start()
@@ -19,20 +31,32 @@ public class PlayerMovements : MonoBehaviour
     {
         if (Input.GetKey(KeyCode.W) && !isMoving)
         {
-            StartCoroutine(MovePlayer(Vector3.up));
+            StartCoroutine(MovePlayer(transform.forward));
         }
         if (Input.GetKey(KeyCode.S) && !isMoving)
         {
-            StartCoroutine(MovePlayer(Vector3.down));
+            StartCoroutine(MovePlayer(-transform.forward));
         }
-        if (Input.GetKey(KeyCode.D) && !isMoving)
+        if (Input.GetKeyDown(KeyCode.D) && !isMoving)
         {
-            StartCoroutine(MovePlayer(Vector3.right));
+            StartCoroutine(FaceDirection(new Vector3(0, 90, 0)));
+
         }
-        if (Input.GetKey(KeyCode.A) && !isMoving)
+        if (Input.GetKeyDown(KeyCode.A) && !isMoving)
         {
-            StartCoroutine(MovePlayer(Vector3.left));
+            StartCoroutine(FaceDirection(new Vector3(0, -90, 0)));
         }
+
+        if (Input.GetAxisRaw("Horizontal") != 0 || Input.GetAxisRaw("Vertical") != 0)
+        {
+            animator.SetBool("IsRunning", true);
+        }
+        else
+        {
+            animator.SetBool("IsRunning", false);
+        }
+
+
     }
 
     public IEnumerator MovePlayer(Vector3 direction)
@@ -41,6 +65,7 @@ public class PlayerMovements : MonoBehaviour
 
         float elapsedTime = 0;
 
+        // origPos = transform.position;
         origPos = transform.position;
         targetPos = origPos + direction;
 
@@ -51,7 +76,25 @@ public class PlayerMovements : MonoBehaviour
             yield return null;
         }
 
+        // transform.position = targetPos;
         transform.position = targetPos;
+
+        isMoving = false;
+    }
+
+    private IEnumerator FaceDirection(Vector3 direction)
+    {
+        isMoving = true;
+        Quaternion initialRotation = transform.rotation;
+
+        float elapsedTime = 0f;
+
+        while (elapsedTime < timeToMove)
+        {
+            elapsedTime += Time.deltaTime;
+            transform.rotation = Quaternion.Lerp(initialRotation, initialRotation * Quaternion.Euler(direction.x, direction.y, direction.z), (elapsedTime / timeToMove));
+            yield return null;
+        }
 
         isMoving = false;
     }
